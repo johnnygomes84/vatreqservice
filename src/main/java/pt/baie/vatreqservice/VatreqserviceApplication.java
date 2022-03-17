759219282d9e8c54cd03ef6bc0a49be7b26c8144
João Gomes <j.gomes@baieuropa.pt>
2022-03-17 14:40:14 +0000

    initial commit

---
+++ src/main/java/pt/baie/vatreqservice/VatreqserviceApplication.java
@@ -0,0 +1,13 @@
+package pt.baie.vatreqservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VatreqserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VatreqserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pt/baie/vatreqservice/VatreqserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.baie.vatreqservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VatreqserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
